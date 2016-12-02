6920bd3d5650193166501de5965ebc16f16e8338
gilalves95 <gilc.hermen.10@gmail.com>
2016-12-02 16:29:39 +0100

    Update evaluation.component.ts

--- app/evaluation/evaluation.component.ts
+++ app/evaluation/evaluation.component.ts
@@ -10,9 +10,16 @@ import { Evaluation } from './evaluation';
 export class EvaluationComponent {
     //variables
     evaluations: Array<Evaluation>;
+    idCount: number;
+
+    addEvaluation(evaluation:number, description:string) {
+        this.evaluations.push(new Evaluation(this.idCount, evaluation, description));
+        this.idCount += 1;
+    }
 
     constructor() {
         this.evaluations = new Array<Evaluation>();
+        this.idCount = 0;
     } 
 
 
